d8640dff6dd43e898f7f9f9f30af08b4d6ad72f3
Pavel Bidenko <pavelbidenko2018@gmail.com>
2020-06-26 10:25:40 +0200

    20-operator-precedence

--- exercises/20-operator-precedence/1-literals/1-coercion-and-comparing.js
+++ exercises/20-operator-precedence/1-literals/1-coercion-and-comparing.js
@@ -11,17 +11,24 @@ console.assert(step0 === expected, 'Step 0');
 /*
 
 */
-const step1 = _;
+const step1 = typeof NaN === typeof NaN;
 console.assert(step1 === expected, 'Step 1');
 
 /*
 
 */
-const step2 = _;
+const step2 = typeof undefined !== typeof null;
 console.assert(step2 === expected, 'Step 2');
 
 /*
 
 */
-const step3 = _;
+const step3 = typeof null !== typeof undefined;
 console.assert(step3 === expected, 'Step 3');
+
+/*
+
+*/
+
+const step4 = typeof undefined !== typeof 'asa';
+console.assert(step4 === expected, 'Step 4');
